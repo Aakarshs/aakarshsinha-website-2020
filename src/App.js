f4bcb70d2172ba7b6093c62ca1f88f9998eb2cfa
Aakarshs <aakarsh.sinha@salliemae.com>
2022-10-28 09:54:54 -0400

    updated rotation

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ export default class Welcome extends React.Component {
                 How is he all of it? Click the button to find out.
             </div>
               <div className="home-button-container">
-                <div className="button" onClick={() => { this.show() }}> Click Here </div>
+                <div className="button" onClick={() => { this.show() }}> Click Here To Find More </div>
                 <div className="button-full"><a className="button-url" target="_blank" href='https://drive.google.com/file/d/1PvzMSNzZi61jjj1Ufw7a1svDeevn4f7O/view?usp=sharing'>Download Resume</a></div>
               </div>
             </div>
