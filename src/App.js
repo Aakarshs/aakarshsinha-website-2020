11e6e5c3fda1ec50ef4d36363cac51a387da4412
Aakarsh Sinha <aakarshs@outlook.com>
2022-11-03 13:31:39 -0400

    Update App.js
    updated resume

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ export default class Welcome extends React.Component {
             </div>
               <div className="home-button-container">
                 <div className="button" onClick={() => { this.show() }}> Click Here To Find More </div>
-                <div className="button-full"><a className="button-url" target="_blank" href='https://drive.google.com/file/d/1PvzMSNzZi61jjj1Ufw7a1svDeevn4f7O/view?usp=sharing'>Download Resume</a></div>
+                <div className="button-full"><a className="button-url" target="_blank" href='https://drive.google.com/file/d/1eLrUg_NZkWPJTQ3sbSCazf0XCQQHYYaR/view?usp=share_link'>Download Resume</a></div>
               </div>
             </div>
           </div>
